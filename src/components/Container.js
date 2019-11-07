2b58f0670067d571ab460480065f415e35ba9c25
John Afolabi <johnafolabi.da@gmail.com>
2019-11-07 17:02:21 +0100

    Refactor to reactstrap

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Form, FormGroup, Label, Input, Row, Col } from "reactstrap";
 import Header from "./Header";
 import ImageInfo from "./ImageInfo";
 import Image from "./Image";
 import axios from "axios";
 
 function Container() {
-  const [date, setDate] = useState("2019-11-06");
+  const [date, setDate] = useState("2019-11-07");
   const [image, setImage] = useState("");
 
   useEffect(() => {
@@ -21,12 +22,32 @@ function Container() {
 
   return (
     <div className="container">
-       <p>Select Date:  <input
+      {/* <p>Select Date:  <input
         type="date"
         min="2012-01-01"
         max="2019-11-06"
         onChange={e => setDate(e.target.value)}
-      ></input></p>
+      ></input></p> */}
+      <Form className="dateSelector">
+        <FormGroup>
+          <Row>
+            <Col sm={{ size: 3, offset: 3 }}>
+              <Label>
+                <h3> Select Date:</h3>
+              </Label>
+            </Col>
+            <Col xs="3">
+              <Input
+                type="date"
+                name="date"
+                id="exampleDate"
+                value={date}
+                onChange={e => setDate(e.target.value)}
+              />
+            </Col>
+          </Row>
+        </FormGroup>
+      </Form>
       <Header title={image.title} />
       <Image url={image.url} title={image.title} />
       <ImageInfo date={image.date} explanation={image.explanation} />
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Spinner } from 'reactstrap';
 
 function Image(props) {
+    const [loaded, setLoaded] = useState(false);
+
+    // useEffect(() => {
+    //     if (loaded === false) return <Spinner style={{ width: '10rem', height: '10rem' }} type="grow" />
+    // }, [loaded])
+
     return (
-        <img className="nasaImage" src={props.url} alt={props.title}></img>
+        <div>
+            <img className="nasaImage" src={props.url} alt={props.title} onLoad={() => setLoaded(true)}></img>
+        </div>
     )
 }
 
